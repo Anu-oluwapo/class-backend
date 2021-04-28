43ba3bb1b1b32880294200ce7b6de4f86ecb9cb4
Anuoluwapo Abolarin <37321402+Anu-oluwapo@users.noreply.github.com>
2021-04-28 09:55:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app  = express();
 
 const http = require('http');
-const port = 2000;
+const port =  process.env.PORT || 3000;
 const server = http.createServer(app);
 
 
